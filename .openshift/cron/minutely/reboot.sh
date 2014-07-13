693703d8b3f96b9c8ac6a9fa364bee9570e59e71
Puteulanus <mj19941230@gmail.com>
2014-07-14 03:05:12 +0800

    合并重启脚本

--- .openshift/cron/minutely/reboot.sh
+++ .openshift/cron/minutely/reboot.sh
@@ -2,6 +2,12 @@
 
 export TZ="Asia/Shanghai"
 
+#判断是否为重启时间
+hour="`date +%H%M`"
+if [ "$hour" = "0010" -o "$hour" = "0020"]; then
+  ctl_all restart
+fi
+
 #配置你的Openshift ssh用户名
 sshid="${OPENSHIFT_APP_UUID}"
 
@@ -9,14 +15,8 @@ sshid="${OPENSHIFT_APP_UUID}"
 curl -I "${OPENSHIFT_APP_DNS}" 2> /dev/null | head -1 | grep -q 200
 s="$?"
 let t="`date +"%M"`%10"
-if [ "$t" -eq 0 ];
-then
-  if [ "$s" != 0 ];
-  then
-      /usr/bin/gear stop 2>&1 /dev/null
-      /usr/bin/gear start 2>&1 /dev/null
-      echo "`date +"%Y-%m-%d %I:%M:%S"` restarted" > "/var/lib/$sshid/app-root/data/web_error.log"
+if [ "$t" -eq 0 ];then
+  if [ "$s" != 0 ];then
+  	ctl_all restart
   fi
-else
-    echo "`date +"%Y-%m-%d %I:%M:%S"` is ok" > "/var/lib/openshift/$sshid/app-root/data/web_run.log"
 fi
