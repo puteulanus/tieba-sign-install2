e214a8151eea708a5d701b4d82e7a7dc480dfc21
Star Brilliant <m13253@hotmail.com>
2014-07-18 21:54:27 +0800

    修改重启脚本

--- .openshift/cron/minutely/reboot.sh
+++ .openshift/cron/minutely/reboot.sh
@@ -2,22 +2,19 @@
 
 export TZ="Asia/Shanghai"
 
-#判断是否为重启时间
+# 每天 00:10 00:20 12:00 各重启一次防止计划任务失败
 hour="`date +%H%M`"
-if [ "$hour" = "0010" -o "$hour" = "0020" ]; then
-  ctl_all restart
+if [ "$hour" = "0010" -o "$hour" = "0020" -o "$hour" = "1200" ]
+then
+  gear restart --all-cartridges
   exit
 fi
 
-#配置你的Openshift ssh用户名
-sshid="${OPENSHIFT_APP_UUID}"
-
-#脚本运行部分
-curl -I "${OPENSHIFT_APP_DNS}" 2> /dev/null | head -1 | grep -q 200
-s="$?"
-let t="`date +"%M"`%10"
-if [ "$t" -eq 0 ];then
-  if [ "$s" != 0 ];then
-  	ctl_all restart
-  fi
+# 十分钟检查一次网站数据库是否正常
+let tenmin="`date +%M` % 10"
+if [ "$tenmin" -eq 0 ]
+then
+  curl -s -I -m 30 http://"${OPENSHIFT_APP_DNS}" | head -1 | grep -q 200 ||
+    gear restart --all-cartridges
+  exit
 fi
