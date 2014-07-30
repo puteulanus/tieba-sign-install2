4ca18a05a0fd147c4513ae39ca13ff3b3c9c1065
Star Brilliant <m13253@hotmail.com>
2014-07-31 02:06:26 +0800

    Modify reboot.sh

--- .openshift/cron/minutely/reboot.sh
+++ .openshift/cron/minutely/reboot.sh
@@ -6,15 +6,21 @@ export TZ="Asia/Shanghai"
 hour="`date +%H%M`"
 if [ "$hour" = "0010" -o "$hour" = "0020" -o "$hour" = "1200" ]
 then
+  echo "Scheduled rebooting at $(date) ..." >&2
   gear restart --all-cartridges
+  echo "Rebooted at $(date) ..." >&2
   exit
 fi
 
 # 十分钟检查一次网站数据库是否正常
-let tenmin="`date +%M` % 10"
-if [ "$tenmin" -eq 0 ]
+min="`date +%M`"
+if [ "${min:1:1}" = "0" ]
 then
-  curl -s -I -m 30 http://"${OPENSHIFT_APP_DNS}" | head -1 | grep -q 200 ||
+  if ! curl -s -I -m 30 http://"${OPENSHIFT_APP_DNS}" | head -1 | grep -q '[23]0[0-9]'
+  then
+    echo "Server not responding, rebooting at $(date) ..." >&2
     gear restart --all-cartridges
-  exit
+    echo "Rebooted at $(date) ..." >&2
+    exit
+  fi
 fi
